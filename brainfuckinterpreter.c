2b70d6b23a5a3ab56a6576e89c31d12643df41a7
luizperes <luiz_peres10@hotmail.com>
2015-12-03 17:18:39 -0800

    Some little bugs, but getting there...

--- brainfuckinterpreter.c
+++ brainfuckinterpreter.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack
+{
+  fpos_t *positionFile;
+  struct stack *previous;
+}Stack;
 
 int main(int argc, char** argv)
 {
@@ -10,14 +17,15 @@ int main(int argc, char** argv)
     return -1;
   
   char array[30000] = {0};
-  char *ptr = array;
-
+  char *ptr = array;  
+  Stack *lastOpenBracket = NULL;
+  
   for(int c = fgetc(f); c != EOF; c = fgetc(f))
   {
     switch(c)
     {
       case '>':
-      { 
+      {
         ++ptr;
         break;
       }
@@ -44,6 +52,52 @@ int main(int argc, char** argv)
       case ',':
       {
         *ptr = getchar();
+        break;
+      }
+      case '[':
+      {
+        if (*ptr == '\0')
+        {
+          while(c != ']' && c != EOF)
+            c = fgetc(f);
+          
+          if (lastOpenBracket != NULL)
+          {
+            fpos_t *curBracket = (fpos_t *) malloc(sizeof(fpos_t));
+            fgetpos(f, curBracket);
+            if (curBracket == lastOpenBracket->positionFile)
+            {
+              Stack *auxPtr = lastOpenBracket;
+              lastOpenBracket = lastOpenBracket->previous;
+              free(auxPtr);
+            }
+          }
+
+          break;
+        }
+
+        if (lastOpenBracket == NULL)
+        {
+          lastOpenBracket = (Stack *) malloc(sizeof(Stack));
+          lastOpenBracket->positionFile = (fpos_t *) malloc(sizeof(fpos_t));
+          fgetpos(f, lastOpenBracket->positionFile);
+          lastOpenBracket->previous = NULL;
+        }
+        else
+        {
+          Stack *auxPtr = lastOpenBracket;
+          lastOpenBracket = (Stack *) malloc(sizeof(Stack));
+          lastOpenBracket->positionFile = (fpos_t *) malloc(sizeof(fpos_t));
+          fgetpos(f, lastOpenBracket->positionFile);
+          lastOpenBracket->previous = auxPtr;
+        }
+        
+        break;
+      }
+      case ']':
+      {
+        fsetpos(f, lastOpenBracket->positionFile);
+        ungetc('[', f); 
         break;
       }
     }  
